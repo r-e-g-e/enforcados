7a77b2a122fa463b74dfcd53458925023cc61b14
jrpolesi <josericardo.kremer@gmail.com>
2022-01-16 16:03:36 -0300

    Import Game component on App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { GlobalStyle } from './globalStyles'
 import { GameProvider } from './contexts/GameContext'
+import { Game } from './components/Game'
+import { SecretWord } from './components/SecretWord';
+import { Gibbet } from './components/Gibbet';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <GameProvider>
-
+        <Game />
       </GameProvider>
     </>
   );
