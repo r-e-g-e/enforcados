54264fc3f92d483d5899f2f36e5affccc6696104
jrpolesi <josericardo.kremer@gmail.com>
2022-01-16 21:23:32 -0300

    Show Persona on error

--- frontend/src/components/Gibbet/index.jsx
+++ frontend/src/components/Gibbet/index.jsx
@@ -1,18 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Container } from "./styles"
 
-export function Gibbet({lifes}) {
+const personaParts = ['persona__leg--right', 'persona__leg--left', 'persona__arm--right', 'persona__arm--left', 'persona__body', 'persona__head']
+
+export function Gibbet({ lifes }) {
+  const [persona, setPersona] = useState([])
+
+  useEffect(() => {
+    const persona = []
+    for (let i = lifes; i < personaParts.length; i++) {
+      persona.push(
+        <div key={i} className={personaParts[i]}></div>
+      )
+    }
+
+    setPersona(persona)
+  }, [lifes])
+
   return (
     <Container>
       <div className="gibbet">
         <div className="gibbet__rope">
           <div className="gibbet__persona">
-            <div className="persona__head"></div>
-            <div className="persona__body"></div>
-            <div className="persona__arm--left"></div>
-            <div className="persona__arm--right"></div>
-            <div className="persona__leg--left"></div>
-            <div className="persona__leg--right"></div>
+            {persona}
           </div>
         </div>
       </div>
