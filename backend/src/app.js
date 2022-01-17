210f0a518d01c4a4d4a501d013bba68290a6da0e
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2022-01-16 21:05:57 -0300

    express removed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,10 @@
-import express from "express"
 import { createServer } from "http"
 import { Server } from "socket.io"
-import cors from "cors"
 
 import { initEvents } from "./events.js"
 
-const app = express()
 
-app.use(cors({
-  origin:"*"
-}))
-
-const server = createServer(app)
+const server = createServer()
 const ioServer = new Server(server, {
   cors:{
     origin:"*"
